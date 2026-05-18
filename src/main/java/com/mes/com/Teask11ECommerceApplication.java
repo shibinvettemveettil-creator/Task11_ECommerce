41e8ab090cd875f3c7094ab9b7d170f700039d44
shibi <shibi@host.docker.internal>
2026-05-18 13:09:57 +0100

    First commit

---
+++ src/main/java/com/mes/com/Teask11ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.mes.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teask11ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Teask11ECommerceApplication.class, args);
+	}
+
+}
